762b5f24f8086ea9808797dcee53b129f80c48e2
florianhamel <florianhamel96@gmail.com>
2020-11-24 13:21:53 +0100

    frame + animation

--- js/script.js
+++ js/script.js
@@ -5,10 +5,28 @@
 // });
 var		width = window.innerWidth + 'px';
 var		height = window.innerHeight + 'px';
-var		size = width + " " + height
+var		size = width + " " + height;
 
-$(".button").click(function() {
+$("document").ready(function()
+{
+	setTimeout(function()
+	{
+		$(".opening").remove();
+		$(".buttons").append("<div class=\"info\">You logged in Florian Hamel's Terminal</div>");
+		$(".buttons").append("<div class=\"button cv\">> CV</div>");
+		$(".buttons").append("<div class=\"button portfolio\">> Portfolio</div>");
+		$(".buttons").append("<div class=\"button about\">> About</div>");
+		$(".buttons").append("<div class=\"button contact\">> Contact</div>");
+	}, 660);
+});
+
+$(".buttons").on("click", ".button", function()
+{
 	$(".transform").toggleClass("transform-active");
 	$(".transform-active").css({"height": height, "width": width, "background-size": size});
-	$(".hiden").toggleClass("hiden-active")
-  });
+	$(".hiden").toggleClass("hiden-active");
+	setTimeout(function()
+	{
+		$(".hiden").remove();
+	}, 500);
+});
